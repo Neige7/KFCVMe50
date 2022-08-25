f6e6fb0acaa4dda140a791b374718aca5d8b7cb0
Neige <2468629609@qq.com>
2022-08-25 20:17:07 +0800

    Initial commit

---
+++ src/main/kotlin/pers/neige/kfcvme50/KFCVMe50.kt
@@ -0,0 +1,11 @@
+package pers.neige.kfcvme50
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object KFCVMe50 : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
